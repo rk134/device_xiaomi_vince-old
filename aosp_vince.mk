68fd57254a9319bc96d6303d8c0b8c8a43f3d707
rahul <41164942+rk134@users.noreply.github.com>
2022-01-22 12:21:15 +0000

    vince: Set TARGET_BOOT_ANIMATION_RES to 1080
    change build FP while at it too.

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -29,7 +29,7 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common ArcanaOS stuff.
 $(call inherit-product, vendor/aosp/config/common.mk)
-TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -50,8 +50,9 @@ BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
+    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
