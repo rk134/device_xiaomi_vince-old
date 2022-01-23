xiaomi-msm8953-devs/android_device_xiaomi_vince

vince: Build custom fingerprint HIDL
Change-Id: Ifd730afca08f87e87709c1f7d37166d9e52e1c18

device.mk
@@ -32,7 +32,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
 
 # Properties
 include $(LOCAL_PATH)/properties.mk

rk134/device_xiaomi_vince-old

Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

device.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fakelogprint \
     libshims_gxfpd \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service.vince
 
 # FM
 PRODUCT_PACKAGES += \