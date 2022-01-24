07d7b6115623278444b00f2e402bf18d2f5ceaf4
rk134 <rahul.kantrapally@gmail.com>
2022-01-24 20:57:27 +0000

    vince: AwakenOS bringup
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vince.mk
+    $(LOCAL_DIR)/awaken_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vince-user \
-    arrow_vince-userdebug \
-    arrow_vince-eng
+    awaken_vince-user \
+    awaken_vince-userdebug \
+    awaken_vince-eng
